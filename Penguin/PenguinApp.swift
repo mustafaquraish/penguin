7202591208e820c26199d66a6d1973968186471e
Mustafa Quraish <mustafaq9@gmail.com>
2025-03-06 21:08:41 -0500

    Initial Commit

---
+++ Penguin/PenguinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenguinApp.swift
+//  Penguin
+//
+//  Created by Mustafa on 2025-03-06.
+//
+
+import SwiftUI
+
+@main
+struct PenguinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PenguinTests/PenguinTests.swift
@@ -0,0 +1,17 @@
+//
+//  PenguinTests.swift
+//  PenguinTests
+//
+//  Created by Mustafa on 2025-03-06.
+//
+
+import Testing
+@testable import Penguin
+
+struct PenguinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
